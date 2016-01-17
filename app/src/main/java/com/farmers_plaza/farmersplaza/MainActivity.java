fee7c8ddaa70b76f490b7fb92c27c1712f243360
whizdummy <jeraldjohnpormon@yahoo.com>
2016-01-17 22:33:07 +0800

    Initial Commit

---
+++ app/src/main/java/com/farmers_plaza/farmersplaza/MainActivity.java
@@ -0,0 +1,13 @@
+package com.farmers_plaza.farmersplaza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
