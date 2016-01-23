2c7929009f661c64512ea79ec57e4aa324a2cccb
whizdummy <jeraldjohnpormon@yahoo.com>
2016-01-24 01:47:57 +0800

    Add registration view

---
+++ app/src/main/java/com/farmers_plaza/farmersplaza/general/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package com.farmers_plaza.farmersplaza.general;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.farmers_plaza.farmersplaza.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
