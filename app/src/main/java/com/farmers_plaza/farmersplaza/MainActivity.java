acb1e9f708c26cbef106229397c3a00eb77eb90e
whizdummy <jeraldjohnpormon@yahoo.com>
2016-01-26 06:35:06 +0800

    Connect Login and Signup on View

--- app/src/main/java/com/farmers_plaza/farmersplaza/MainActivity.java
+++ app/src/main/java/com/farmers_plaza/farmersplaza/MainActivity.java
@@ -1,13 +1,54 @@
 package com.farmers_plaza.farmersplaza;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.farmers_plaza.farmersplaza.controllers.general.RegistrationActivity;
+import com.farmers_plaza.farmersplaza.farmer.HomeScreenActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin, btnRegister;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        btnLoginOnClick();
+        btnSignupOnClick();
+    }
+
+    private void btnLoginOnClick() {
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logic goes here
+                showIntent(HomeScreenActivity.class);
+            }
+        });
+    }
+
+    private void btnSignupOnClick() {
+        btnRegister = (Button) findViewById(R.id.btnSignUp);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showIntent(RegistrationActivity.class);
+            }
+        });
+    }
+
+    private void showIntent(Class className) {
+        intent = new Intent(MainActivity.this, className);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/farmers_plaza/farmersplaza/farmer/HomeScreenActivity.java
@@ -0,0 +1,14 @@
+package com.farmers_plaza.farmersplaza.farmer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.farmers_plaza.farmersplaza.R;
+
+public class HomeScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
