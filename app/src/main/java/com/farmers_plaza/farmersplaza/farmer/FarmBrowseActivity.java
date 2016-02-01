1d89fae2e38c074cd0ba5d19ffde332d616da881
whizdummy <jeraldjohnpormon@yahoo.com>
2016-02-01 08:49:18 +0800

    Add "add farm" button

--- app/src/main/java/com/farmers_plaza/farmersplaza/farmer/FarmBrowseActivity.java
+++ app/src/main/java/com/farmers_plaza/farmersplaza/farmer/FarmBrowseActivity.java
@@ -1,24 +1,44 @@
 package com.farmers_plaza.farmersplaza.farmer;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.farmers_plaza.farmersplaza.R;
 import com.farmers_plaza.farmersplaza.controllers.general.ToolbarSetup;
 
 public class FarmBrowseActivity extends AppCompatActivity {
     ToolbarSetup toolbarSetup;
+    Button btnAddFarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_farms);
+        setUp();
+        addFarmClick();
+    }
+
+    private void addFarmClick() {
+        btnAddFarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FarmBrowseActivity.this, AddFarmActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setUp() {
+        btnAddFarm = (Button) findViewById(R.id.btn_add_farm);
         setupToolbar();
     }
 
     private void setupToolbar() {
-        toolbarSetup = new ToolbarSetup(findViewById(R.id.toolbar), this, this);
+        toolbarSetup = new ToolbarSetup(findViewById(R.id.toolbar_farm), this, this);
         toolbarSetup.initialize(getString(R.string.browse_farm_toolbar_title), Color.WHITE);
     }
 }
--- app/src/main/java/com/farmers_plaza/farmersplaza/farmer/IncomeActivity.java
+++ app/src/main/java/com/farmers_plaza/farmersplaza/farmer/IncomeActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
