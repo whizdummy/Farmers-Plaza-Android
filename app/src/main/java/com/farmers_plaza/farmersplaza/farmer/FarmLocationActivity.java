3098c4d561e1cd8d8fdc46433d20933782e89588
whizdummy <jeraldjohnpormon@yahoo.com>
2016-02-01 12:13:03 +0800

    Map is now working

--- app/src/main/java/com/farmers_plaza/farmersplaza/farmer/FarmLocationActivity.java
+++ app/src/main/java/com/farmers_plaza/farmersplaza/farmer/FarmLocationActivity.java
@@ -1,30 +1,77 @@
 package com.farmers_plaza.farmersplaza.farmer;
 
-import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.farmers_plaza.farmersplaza.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
-public class FarmLocationActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
 
-    private GoogleMap mMap;
+public class FarmLocationActivity extends Activity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private MapView mapView;
+    private ParseQuery<ParseObject> farmQuery;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_location);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapView = (MapView) findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        try {
+            MapsInitializer.initialize(this);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+        mapView.getMapAsync(this);
+        getFarmersFarm();
+    }
+
+    private void getFarmersFarm() {
+        farmQuery = ParseQuery.getQuery("Farm");
+        farmQuery.whereEqualTo("farmer", ParseUser.getCurrentUser());
+        (new FetchFarm()).execute();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 
     /**
      * Manipulates the map once available.
@@ -37,11 +84,36 @@ public class FarmLocationActivity extends FragmentActivity implements OnMapReady
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        map = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private class FetchFarm extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            farmQuery.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> objects, ParseException e) {
+                    if(e == null) {
+                        LatLng farmLocation;
+                        for(ParseObject object : objects) {
+                            farmLocation =
+                                    new LatLng(object.getParseGeoPoint("farmerGeoPoint").getLatitude()
+                                    , object.getParseGeoPoint("farmerGeoPoint").getLongitude());
+                            marker = map.addMarker(new MarkerOptions().position(farmLocation)
+                                    .title(object.getString("farmName")));
+
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(farmLocation, 15f));
+                        }
+                    }
+                }
+            });
+
+            return null;
+        }
     }
 }
